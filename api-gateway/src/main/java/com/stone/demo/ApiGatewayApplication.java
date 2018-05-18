bbc590346d26956a19ec9de5b9c7864880c76896
stone <stone.zhuang@sunvalley.com.cn>
2018-05-18 14:44:11 +0800

    配置、网关添加

---
+++ api-gateway/src/main/java/com/stone/demo/ApiGatewayApplication.java
@@ -0,0 +1,25 @@
+package com.stone.demo;
+
+import com.stone.demo.filter.AccessFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter(){
+		return new AccessFilter();
+	}
+}
---
+++ api-gateway/src/main/java/com/stone/demo/filter/AccessFilter.java
@@ -0,0 +1,37 @@
+package com.stone.demo.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.http.protocol.RequestContent;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author stone.zhuang
+ * @date 2018/4/27
+ */
+public class AccessFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        System.out.println(request.getRequestURI());
+        return null;
+    }
+}
